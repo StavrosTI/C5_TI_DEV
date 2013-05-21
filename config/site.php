665f7f6a92d71c255e0f8d7ebc0976538a315ff2
Stavros Louris <stavros.louris@travimp.com>
2013-05-21 11:42:49 -0400

    Initial Commit
    Empty C5 install

---
+++ config/site.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'concrete_ti');
+define('DB_PASSWORD', 'ufAhGyHVUWX6WjLj');
+define('DB_DATABASE', 'concrete_ti');
+define('PASSWORD_SALT', 'FuXwgTKEApI8tlJQYn8G5DIVprHGmzkk3NiNfExV9XkqiCB00EXQJQv7a2g7kODu');
